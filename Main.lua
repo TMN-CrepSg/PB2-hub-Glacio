a7c2feb483d6a6b517012bd740635df116c4ec82
TMN-CrepSg <mannistur6@gmail.com>
2025-05-13 21:19:48 +0200

    Main.lua

--- Main.lua
+++ Main.lua
@@ -25,12 +25,12 @@ local Window = Rayfield:CreateWindow({
       Key = {"https://pastebin.com/raw/8NUybKs3"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
    }
 })
-local MainTab = Window:CreateTab("Credis", nil)
+local MainTab = Window:CreateTab("🏠 Home", nil) -- Title, Image
 local MainSection = MainTab:CreateSection("Main")
 
 Rayfield:Notify({
    Title = "You executed the script",
-   Content = "PB2 Glacio_tv",
+   Content = "Very cool gui",
    Duration = 5,
    Image = 13047715178,
    Actions = { -- Notification Buttons
@@ -42,9 +42,6 @@ Rayfield:Notify({
    },
 },
 })
-
-
-local MainTab = Window:CreateTab("LocalPlayer", nil) -- Title, Image
 
 local Button = MainTab:CreateButton({
    Name = "Infinite Jump Toggle",
@@ -97,6 +94,37 @@ local Slider = MainTab:CreateSlider({
    Flag = "sliderjp", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
    Callback = function(Value)
         game.Players.LocalPlayer.Character.Humanoid.JumpPower = (Value)
+   end,
+})
+
+local Dropdown = MainTab:CreateDropdown({
+   Name = "Select Area",
+   Options = {"Starter World","Pirate Island","Pineapple Paradise"},
+   CurrentOption = {"Starter World"},
+   MultipleOptions = false,
+   Flag = "dropdownarea", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+   Callback = function(Option)
+        print(Option)
+   end,
+})
+
+local Input = MainTab:CreateInput({
+   Name = "Walkspeed",
+   PlaceholderText = "1-500",
+   RemoveTextAfterFocusLost = true,
+   Callback = function(Text)
+        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = (Text)
+   end,
+})
+
+local OtherSection = MainTab:CreateSection("Other")
+
+local Toggle = MainTab:CreateToggle({
+   Name = "Auto Farm",
+   CurrentValue = false,
+   Flag = "Toggle1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+   Callback = function(Value)
+        print("FARMING")
    end,
 })
 
