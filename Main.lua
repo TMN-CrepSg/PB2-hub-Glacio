5ccb81225235cc7a8e135d8c4d08312cf2f3df28
TMN-CrepSg <mannistur6@gmail.com>
2025-05-13 21:51:19 +0200

    Main.lua

--- Main.lua
+++ Main.lua
@@ -20,13 +20,12 @@ local Window = Rayfield:CreateWindow({
       Subtitle = "Key System",
       Note = "Key In Discord Server",
       FileName = "PB2 Glacio_tv HubKey1", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
-      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+      SaveKey = false, -- The user's key will be saved, but if you change the key, they will be unable to use your script
       GrabKeyFromSite = true, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
       Key = {"https://pastebin.com/raw/8NUybKs3"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
    }
 })
 local MainTab = Window:CreateTab("Credis", nil) -- Title, Image
-local MainSection = MainTab:CreateSection("Main")
 
 Rayfield:Notify({
    Title = "You executed the script",
@@ -44,7 +43,7 @@ Rayfield:Notify({
 })
 
 
-local TPTab = Window:CreateTab("LocalPlayer", nil) -- Title, Image
+local LocalPlayer = Window:CreateTab("LocalPlayer", nil) -- Title, Image
 
 local Slider = MainTab:CreateSlider({
    Name = "WalkSpeed Slider",
