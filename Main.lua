3123dce43b4b9150a0c62aad244227d96beec811
TMN-CrepSg <mannistur6@gmail.com>
2025-05-13 21:49:04 +0200

    Main.lua

--- Main.lua
+++ Main.lua
@@ -20,7 +20,7 @@ local Window = Rayfield:CreateWindow({
       Subtitle = "Key System",
       Note = "Key In Discord Server",
       FileName = "PB2 Glacio_tv HubKey1", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
-      SaveKey = false, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
       GrabKeyFromSite = true, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
       Key = {"https://pastebin.com/raw/8NUybKs3"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
    }
